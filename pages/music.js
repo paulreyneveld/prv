b2029ef3dcc22fbc08327acc77de862cd9abb770
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-05-12 14:57:14 -0700

    Testing the audio tag

--- pages/music.js
+++ pages/music.js
@@ -4,6 +4,8 @@ const Music = () => {
     return (
         <div className={styles.container}>
             <h1>Testing</h1>
+            <audio src="../public/" controls />
+
         </div>
     )
 }
