48cce76597c0afacbf0651c32bd66b43bc59904f
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-05-12 14:55:05 -0700

    Added a new page and set up Link

---
+++ pages/music.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+
+const Music = () => {
+    return (
+        <div className={styles.container}>
+            <h1>Testing</h1>
+        </div>
+    )
+}
+
+export default Music
