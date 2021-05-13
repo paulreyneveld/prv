cbef5864e62d2f3da464110364cb383d8af2c5c3
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-05-13 14:08:33 -0700

    Added a dummy blog post

---
+++ pages/blog/[id].js
@@ -0,0 +1,45 @@
+// Obviously get data from somewhere
+
+const Blog = () => {
+    return (
+        <>
+        <h1>Test Blog</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum ante non facilisis pellentesque. 
+        Mauris tempus enim elit, ut placerat ante dictum ac. Aenean aliquam nisl leo, eget eleifend neque ultricies eget. 
+        Curabitur vulputate pretium ligula, vitae rhoncus ipsum ultricies eget. Sed ac commodo lacus. Duis consectetur risus vel 
+        leo suscipit ullamcorper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis 
+        egestas. Etiam sit amet bibendum sem.
+
+        Nulla facilisi. Quisque ac aliquet erat, sit amet pharetra nibh. Proin fermentum elementum nunc sit amet volutpat.
+        Vivamus eget est elit. Mauris vel pellentesque metus, eu feugiat tellus. Nullam vitae nulla vel neque faucibus 
+        imperdiet. Nullam fermentum in tellus id elementum. Maecenas accumsan auctor molestie. Maecenas vel dui at eros 
+        tincidunt cursus. Vestibulum elit purus, aliquet pellentesque molestie eu, bibendum quis orci. Vestibulum 
+        eleifend eros quis enim consectetur vulputate. Integer aliquam augue vel felis laoreet bibendum. Suspendisse 
+        potenti. Integer leo nibh, pellentesque vel luctus quis, feugiat quis lorem. Vestibulum vitae enim egestas, 
+        pellentesque orci eu, dignissim elit. Ut nec ante non ante vehicula tincidunt vitae eget diam.
+
+        Maecenas elit tortor, accumsan a dolor in, hendrerit suscipit tortor. Curabitur ac accumsan leo, a lobortis felis. 
+        Sed id suscipit enim. Suspendisse eu tempor justo. Sed sit amet posuere urna. Nullam mollis purus in lorem hendrerit 
+        rhoncus. Phasellus vel purus at lorem pellentesque commodo. In eu arcu quam. Etiam commodo sem rhoncus, volutpat risus 
+        eget, ornare metus. Proin velit sem, ultrices sit amet nunc ut, tempor fringilla justo. Pellentesque habitant morbi 
+        tristique senectus et netus et malesuada fames ac turpis egestas. Aenean eu nulla odio.
+
+        Curabitur imperdiet sed dolor ac pharetra. Donec fermentum erat ut mauris aliquam accumsan. Ut pulvinar, 
+        eros ut interdum pellentesque, erat libero hendrerit augue, et malesuada justo orci non ipsum. Sed 
+        sollicitudin pulvinar mauris in fringilla. Cras id turpis neque. In lacus sem, efficitur non lacinia sed,
+        elementum maximus nulla. Integer felis justo, luctus sed luctus a, euismod dictum velit. Donec vestibulum 
+        dapibus lacus, sed dapibus neque. Aliquam mollis tortor nec pulvinar ultricies. Integer suscipit lorem et 
+        felis lacinia, eget rutrum ipsum vehicula. Suspendisse tincidunt, enim eget sollicitudin sagittis, lacus 
+        tortor commodo neque, eu sagittis eros dolor ac erat. Vestibulum suscipit orci pharetra neque tincidunt 
+        suscipit. Ut scelerisque vestibulum elit cursus condimentum.
+
+        Phasellus laoreet nisl enim, nec fringilla nisl tempor quis. Vestibulum sit amet libero ac turpis 
+        vestibulum vulputate. Nulla elementum vehicula massa ut tempus. Phasellus eget dolor purus. Nulla mattis 
+        tortor sit amet tortor posuere, at feugiat diam bibendum. Praesent enim eros, ornare at ullamcorper a, 
+        dictum laoreet arcu. Suspendisse accumsan laoreet diam, ut volutpat nisi viverra et. Ut ut fringilla leo. 
+        Nulla finibus nunc dui, vitae sollicitudin arcu ornare vitae. Pellentesque elementum facilisis felis, 
+        dignissim dapibus est porta a. Etiam sed nunc in erat faucibus volutpat sed ac nisl.
+        </p>
+        </>
+    )
+}
